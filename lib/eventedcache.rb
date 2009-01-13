b5be6243c790d16a3898b04178d319157e77f2aa
Cliff Moon <cliff@moonpolysoft.com>
2009-01-12 23:22:55 -0800

    requires

--- lib/eventedcache.rb
+++ lib/eventedcache.rb
@@ -1,5 +1,7 @@
 ROOT = File.expand_path(File.dirname(__FILE__) + "/../")
 
+require 'rubygems'
+require 'eventmachine'
 
 require "#{ROOT}/ext/memcache_protocol"
-
+require "#{ROOT}/lib/eventedcache/connection"
